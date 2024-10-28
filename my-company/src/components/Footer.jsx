1c567902165a254db6a49f15ed7278c3a1474488
NgaruiyaFaith <wangarifaith2020@gmail.com>
2024-10-28 16:07:13 +0300

    Build a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#333', color: 'white', marginTop: '20px' }}>
+        <p>&copy; 2023 My Company</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
