ed25af6529e3f08cbd123d62877caa5177fba647
NgaruiyaFaith <wangarifaith2020@gmail.com>
2024-10-28 14:38:24 +0300

    modified tailwind.config

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', 
+    './public/index.html'
+  ], 
+  darkMode: 'media', // Or 'class' for manual control
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
